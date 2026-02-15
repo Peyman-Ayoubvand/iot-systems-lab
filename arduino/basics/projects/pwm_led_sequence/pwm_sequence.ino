a8f7136bb219318ea64d87cbba7c135533beeb12
Peyman Ayoubvand <106865606+Peyman-Ayoubvand@users.noreply.github.com>
2026-02-15 18:44:39 +0100

    Update pwm_sequence.ino

--- arduino/basics/projects/pwm_led_sequence/pwm_sequence.ino
+++ arduino/basics/projects/pwm_led_sequence/pwm_sequence.ino
@@ -1,1 +1,53 @@
+/*
+  Project: PWM LED Sequence
+  Section: Arduino Basics
+  Description: Simple LED sequence using digital output pins.
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(11,OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(3,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  analogWrite(11,255);
+  delay(200);
+  analogWrite(11,20);
+  analogWrite(10,255);
+  delay(200);
+  analogWrite(10,20);
+  analogWrite(9,255);
+  delay(200);
+  analogWrite(9,20);
+  analogWrite(6,255);
+  delay(200);
+  analogWrite(6,20);
+  analogWrite(5,255);
+  delay(200);
+  analogWrite(5,20);
+  analogWrite(3,255);
+  delay(200);
+  analogWrite(3,20);
+  analogWrite(5,255);
+  delay(200);
+  analogWrite(5,20);
+  analogWrite(6,255);
+  delay(200);
+  analogWrite(6,20);
+  analogWrite(9,255);
+  delay(200);
+  analogWrite(9,20);
+  analogWrite(10,255);
+  delay(200);
+  analogWrite(10,20);
+  analogWrite(11,255);
+  delay(200);
+  analogWrite(11,20);
+  }
 
