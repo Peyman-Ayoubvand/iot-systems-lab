f55ac55b8271d549a49af7a9111982c733327e94
Peyman Ayoubvand <106865606+Peyman-Ayoubvand@users.noreply.github.com>
2026-03-17 10:35:55 +0100

    Implement IR control for robot car movements
    Refactored car control logic to respond to IR commands and removed repetitive movement code.

--- arduino/basics/projects/robot_car_control/car_control.ino
+++ arduino/basics/projects/robot_car_control/car_control.ino
@@ -1,5 +1,12 @@
+#define DECODE_NEC
+#include <IRremote.hpp>
+
+int globalSpeed = 255;
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  IrReceiver.begin(11, true);
   //Connected To Motor A
   pinMode(3,OUTPUT);
   pinMode(5,OUTPUT);
@@ -7,53 +14,48 @@ void setup() {
   pinMode(6,OUTPUT);
   pinMode(9,OUTPUT);
 
-  Serial.begin(9600);
+  stop();
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  forwards(255);
-  delay(1000);
-  stop();
-  delay(1000);
-
-  left(200);
-  delay(250);
-  stop();
-  delay(1000);
-//--------------------
- forwards(255);
-  delay(1000);
-  stop();
-  delay(1000);
-
-  left(200);
-  delay(250);
-  stop();
-  delay(1000);
-//--------------------
- forwards(255);
-  delay(1000);
-  stop();
-  delay(1000);
-
-  left(200);
-  delay(250);
-  stop();
-  delay(1000);
-//--------------------
- forwards(255);
-  delay(1000);
-  stop();
-  delay(1000);
-
-  left(200);
-  delay(250);
-  stop();
-  delay(1000);
-//--------------------
- 
+  if (IrReceiver.decode()){
+      IrReceiver.resume();
+      // IrReceiver.printIRResultShort(&Serial);
+      switch (IrReceiver.decodedIRData.command){
+        case 0x18:
+         forwards(globalSpeed);
+         
+        break;
+        case 0x1c:
+          stop();
+        break;
+        case 0x5A:
+          right(globalSpeed);
+        break;
+         case 0x52:
+            backwards(globalSpeed);
+         break;
+         case 0x8:
+           left(globalSpeed);
+         break;
+         case 0x15:
+           globalSpeed = globalSpeed - 15;
+           if (globalSpeed < 180)globalSpeed = 180;
+           Serial.println(globalSpeed);
+         break;
+         case 0x9:
+          globalspeed = globalspeed + 10;
+          if (globalspeed > 255){
+          globalspeed = 255;
+          Serial.println(globalSpeed);
+          break;
+          }
+    
+         break;
+      }
+   }
 }
 
 void forwards(int speed){
@@ -109,3 +111,4 @@ void stop(){
   digitalWrite(6,LOW);
   digitalWrite(9,LOW);
 }
+
